5df3ec2c4eb0a17c4b0d251b0e9161e6579ddc22
khaled-badenjki <m.k.badenjki2@gmail.com>
2023-06-12 19:52:49 +0200

    add singlify middleware

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Cilo = require('./lib/cilo');
 const tenantify = require('./lib/tenantify');
+const singlify = require('./lib/singlify');
 
 module.exports = {
   Cilo,
-  tenantify
+  tenantify,
+  singlify
 }
